6c7753d55b1fed908d78e990a8fe45ae0302339e
Archit Joshi <archit.joshi@anarock.com>
2025-09-19 19:54:27 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Archit  Joshi on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
